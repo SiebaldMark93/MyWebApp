30d39e7aa2b5a36e29f321edd3b9ac85c21b07c5
Mark Siebald <siebald.mark93@gmail.com>
2022-11-04 14:46:53 -0500

    feat: creating and testing add function

--- src/test/java/com/codeup/mywebapp/UserRepositoryTest.java
+++ src/test/java/com/codeup/mywebapp/UserRepositoryTest.java
@@ -1,10 +1,15 @@
 package com.codeup.mywebapp;
 
+import com.codeup.mywebapp.user.User;
+import com.codeup.mywebapp.user.UserRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.annotation.Rollback;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
@@ -15,6 +20,14 @@ public class UserRepositoryTest {
     @Test
     public void testAddNew() {
         User user = new User();
-        user.setEmail
+        user.setEmail("mark1@gmail.com");
+        user.setPassword("mark123456");
+        user.setFirstName("Mark");
+        user.setLastName("Siebald");
+
+        User savedUser = repo.save(user);
+
+        assertThat(savedUser).isNotNull();
+        assertThat(savedUser.getId()).isGreaterThan(0);
     }
 }
